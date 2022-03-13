3055ffd35702f8110d993635782989d6be74a058
Hygor Costa <cost0142@algonquinlive.com>
2022-03-13 00:29:46 -0500

    add some texts

---
+++ flutter_first_application_hygor_costa/android/app/src/main/kotlin/com/example/flutter_first_application_hygor_costa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_first_application_hygor_costa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_first_application_hygor_costa/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  Column(
+  children: <Widget>[
+    Text('Deliver features faster', textDirection: TextDirection.ltr,),
+
+    Text(
+      'Craft beautiful UIs',
+      textDirection: TextDirection.ltr,
+      ),
+        Text(
+      'by my Self',
+      textDirection: TextDirection.ltr,
+      ),
+      Column(
+        children: [
+          Text(
+      'by my Self',
+      textDirection: TextDirection.ltr,
+      ), 
+        ],
+      )
+    
+    
+  ],
+),
+  );
+
+
+
