3c474821d18d84381be195b1c70f97f2ddb3b77c
Hygor Costa <cost0142@algonquinlive.com>
2022-03-14 17:02:45 -0400

    add list

--- flutter_first_application_hygor_costa/lib/main.dart
+++ flutter_first_application_hygor_costa/lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
+    body: Column( 
+    children: <Widget>[
+    Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text("100.0"),
+        subtitle: Text("1000"),
+      )
+    ),
+    Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text("200.0"),
+        subtitle: Text("1000"),
+      )
+    ),
+    ],
+    ),
     appBar: AppBar(title: Text("Transfers"),),
     floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Add your onPressed code here!
         },
-        backgroundColor: Color.fromARGB(255, 40, 182, 193),
-        child: const Icon(Icons.navigation),
+        backgroundColor: Color.fromARGB(232, 0, 186, 254),
+        child: const Icon(Icons.add),
     ),
   ),
 ));
