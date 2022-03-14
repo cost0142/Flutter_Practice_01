0b50e5682709a8c75f286a3180a290f177ca9b28
Hygor Costa <cost0142@algonquinlive.com>
2022-03-14 17:16:31 -0400

    add class TransferList

--- flutter_first_application_hygor_costa/lib/main.dart
+++ flutter_first_application_hygor_costa/lib/main.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    body: Column( 
-    children: <Widget>[
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text("100.0"),
-        subtitle: Text("1000"),
-      )
-    ),
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text("200.0"),
-        subtitle: Text("1000"),
-      )
-    ),
-    ],
-    ),
-    appBar: AppBar(title: Text("Transfers"),),
-    floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        backgroundColor: Color.fromARGB(232, 0, 186, 254),
-        child: const Icon(Icons.add),
-    ),
-  ),
-));
+      home: Scaffold(
+        body: TransferList(),
+          appBar: AppBar(
+          title: Text("Transfers"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          backgroundColor: Color.fromARGB(232, 0, 186, 254),
+          child: const Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   
+    return Column(
+          children: <Widget>[
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text("100.0"),
+              subtitle: Text("1000"),
+            )),
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text("200.0"),
+              subtitle: Text("1000"),
+            )),
+          ],
+        );
+  }
+}
+
