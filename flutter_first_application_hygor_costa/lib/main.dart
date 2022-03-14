b9def8a84171aa891f19d480be86da332b6000f2
Hygor Costa <cost0142@algonquinlive.com>
2022-03-14 16:39:54 -0400

    add Navegation  button

--- flutter_first_application_hygor_costa/lib/main.dart
+++ flutter_first_application_hygor_costa/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    appBar: AppBar(),
+    appBar: AppBar(title: Text("Transfers"),),
+    floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Color.fromARGB(255, 40, 182, 193),
+        child: const Icon(Icons.navigation),
+    ),
   ),
 ));
